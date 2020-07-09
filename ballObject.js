a281bab606b6b6561b9385521351ae399a3dced0
amaaniscool99 <66651819+amaaniscool99@users.noreply.github.com>
2020-07-09 15:54:10 -0400

    Add files via upload

---
+++ ballObject.js
@@ -0,0 +1,28 @@
+class Ball {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic: false,
+          restitution:0.7,
+          friction:0.5,
+          density:1.2
+      }
+      this.body = Bodies.rectangle(x, y, 20, 20, options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
